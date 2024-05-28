1006dccd8a6213b8d223cc0cc9900a79e9577d85
Asha Luckins <ashaluckinsanoop@gmail.com>
2024-05-28 17:32:53 -0400

    make volume functionality wok

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const video=document.querySelector('video');
 const progressRange=document.querySelector('.progress-range');
 const progressBar=document.querySelector('.progress-bar');
 const playBtn=document.getElementById("play-btn");
-const volumeIcon=document.querySelector('.volume-icon');
+const volumeIcon=document.querySelector('#volume-icon');
 const volumeRange=document.querySelector('.volume-range');
 const volumeBar=document.querySelector('.volume-bar');
 const currentTime=document.querySelector(".time-elapsed");
@@ -56,7 +56,55 @@ function setProgressBar(e){
 
 // Volume Controls --------------------------- //
 
+let lastVolume=1;
 
+//CHnage volumeIcon classes according to the volume
+function changeVolumeIcon(volume){
+    if(volume>0.7){
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+    }else if(volume<=0.7 && volume>0){
+        volumeIcon.classList.add('fas', 'fa-volume-down');
+    }else if(volume===0){
+        volumeIcon.classList.add('fas', 'fa-volume-off');
+    }
+    volumeIcon.setAttribute('title', 'Mute');
+}
+
+//Change volume
+function changeVolume(e){
+    let volume=e.offsetX/volumeRange.offsetWidth;
+    if(volume<0.1){
+        volume=0;
+    }
+    if(volume>0.9){
+        volume=1;
+    }
+    volumeBar.style.width=`${volume*100}%`;
+    video.volume=volume;
+    lastVolume=volume;
+    //set volume icon
+    volumeIcon.className='';
+   changeVolumeIcon(volume);
+    
+}
+
+
+
+//function to mute and unmute
+function toggleMute(){
+    if(video.volume){
+        lastVolume=video.volume;
+        video.volume=0;
+        volumeBar.style.width=0;
+        volumeIcon.className='';
+        volumeIcon.classList.add('fas', 'fa-volume-mute');
+        volumeIcon.setAttribute('title', 'UnMute');
+    }else{
+        video.volume=lastVolume;
+        volumeBar.style.width=`${video.volume*100}%`;
+        changeVolumeIcon(video.volume);
+    }
+}
 
 // Change Playback Speed -------------------- //
 
@@ -72,3 +120,5 @@ video.addEventListener('ended',showPlayIcon);
 video.addEventListener('timeupdate', updateProgressBar);
 video.addEventListener('canplay', updateProgressBar);
 progressRange.addEventListener('click', setProgressBar);
+volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
